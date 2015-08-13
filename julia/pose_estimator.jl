6219c1059d3fe7b16447f02e782c5e4816c2efcf
sonicair <ishitah.takeshi@gmail.com>
2015-08-13 13:33:12 +0900

    Create multiple executables

--- julia/pose_estimator.jl
+++ julia/pose_estimator.jl
@@ -1,9 +1,26 @@
+# Copyright (C) 2015 Ishita Takeshi
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 module PoseEstimator
+
 
 export Candidate, create_estimator, estimate
 
 
-type Part
+type Point
     x::Uint64
     y::Uint64
 end
@@ -11,7 +28,7 @@ end
 
 type CCandidate
     size::Uint64
-    parts::Ptr{Ptr{Part}}
+    parts::Ptr{Ptr{Point}}
     confidence::Ptr{Float32}
 end
 
@@ -73,7 +90,6 @@ function free_candidates(candidates)
     ccall((:free_candidates, "./pose/lib/libPartsBasedDetector.so"),
           Void, (Ptr{CCandidates},), candidates)
 end
-
 
 
 type Candidate
--- julia/test_pose_estimator.jl
+++ julia/test_pose_estimator.jl
@@ -1,16 +1,43 @@
 include("pose_estimator.jl")
 
+#using Images
+using PyCall
+pyinitialize("python3")
+
+using PyPlot
+pygui(:tk)
+
 using PoseEstimator
 
-model_filename = "./pose/matlab/demo_model.mat"
-estimator = create_estimator(model_filename)
-candidates = estimate(estimator, "./pose/dataset/images/2007_000480.jpg")
 
+model_filename = "./pose/matlab/demo_model.mat"
+image_filename = "./pose/dataset/images/2007_001423.jpg"
+
+
+estimator = create_estimator(model_filename)
+candidates = estimate(estimator, image_filename)
+
+confidence = Array(Float32, length(candidates))
+for i in 1:length(candidates)
+    confidence[i] = abs(sum(candidates[i].confidence))
+end
+
+index = indmax(confidence)
+#sort(candidates, by=x->sum(x.confidence)
+
+
+image = PyPlot.imread(image_filename)
+PyPlot.imshow(image)
+
+for candidate in candidates
+    for point in points
+        PyPlot.scatter([point.x], [point.y])
+    end
+end
+#
+PyPlot.savefig("./fig1.jpg")
 #for candidate in candidates
-#    println(candidate.confidence)
+#    println(candidate.parts)
 #end
-#
-#using Base.Sort
-#
-#sort(candidates, by=x->sum(x.confidence))
-##println(candidates)
+
+#println(candidates)
