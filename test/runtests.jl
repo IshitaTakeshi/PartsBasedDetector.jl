b8c63e54d682ceae5f61abdf036e1b66db24d8cd
Ishita Takeshi <ishitah.takeshi@gmail.com>
2016-04-09 16:31:39 +0900

    Cleanup tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,53 +1,17 @@
-include("estimator.jl")
-include("image.jl")
-
 using Images: data, Image, load
 using PyCall
 
-using PoseEstimator: create_estimator, estimate
+using PartsBasedDetector: make_estimator, estimate
 
 
-model_path = "./pose/models/xml/PersonINRIA_9parts.xml"
-image_path = "./pose/dataset/dataset/INRIAPerson/Train/pos/person_044.png"
-#image_path = "pose/dataset/dataset/INRIAPerson/Train/pos/crop001012.png"
-
-
-function get_bounding_box_of_person(candidate)
-    xlist = slice(candidate.parts, 1, :)
-    ylist = slice(candidate.parts, 2, :)
-    right = maximum(xlist)
-    left = minimum(xlist)
-    top = minimum(ylist)
-    bottom = maximum(ylist)
-    return (left, right, top, bottom)
-end
-
-
-default_size = (100, 180)
-
-estimator = create_estimator(model_path)
-candidates = estimate(estimator, image_path, 3)
-for c in candidates
-    println("size = $(c.size)")
-    println("parts = $(c.parts)")
-    println("confidence = $(c.confidence)")
-end
-
-parts = candidates[1].parts
+estimator = make_estimator()
+candidates = estimate(estimator, image_path, n_candidates = 3)
 
 @pyimport matplotlib.pyplot as plt
-x, y = parts[:, 1], parts[:, 2]
-println("x = $x")
-println("y = $y")
 image = plt.imread(image_path)
 plt.imshow(image)
-plt.plot(x, y, "ko")
-plt.show()
-#ground truth
-#annotation = readcsv(open("pose/dataset/dataset/fashionista/annotation10.txt"))
 
-#image = load(image_path)
-#
-#box = get_bounding_box_of_person(candidates[1])
-#image = imcrop(image, box)
-#image = Images.imresize(image, default_size)
+parts = candidates[1].parts
+x, y = parts[:, 1], parts[:, 2]
+plt.plot(x, y, color="r", marker="o", ls="")
+plt.show()
