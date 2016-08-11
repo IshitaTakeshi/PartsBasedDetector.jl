4256c89ed3d96480ec1887aa1c3825f38c1fc92f
IshitaTakeshi <ishitah.takeshi@gmail.com>
2016-08-12 01:34:38 +0900

    Plot estimated joints when running Pkg.test

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,14 +2,26 @@ ENV["PYTHON"] = "/usr/bin/python3"
 using PyCall
 using PartsBasedDetector: make_estimator, estimate
 
-
-filename = "dress.jpg"
 url = "http://images0.chictopia.com/photos/districtofchic/9466442894/" *
       "black-skinny-jeans-dl1961-jeans-sky-blue-pixie-market-shirt_400.jpg"
 
-filename_png = replace(filename, ".jpg", ".png")
+filename = basename(url)
 run(`wget --continue $url -O $filename`)
-run(`convert $filename $filename_png`)
 
-estimator = make_estimator()
+root = Pkg.dir("PartsBasedDetector")
+model_path = joinpath(root, "deps", "src", "pbd", "models", "mat", "lsp.mat")
+
+estimator = make_estimator(model_path)
 candidates = estimate(estimator, filename, n_candidates = 3)
+
+@pyimport matplotlib.pyplot as plt
+@pyimport scipy.misc as misc
+
+parts = candidates[1].parts
+image = misc.imread(filename)
+plt.imshow(image)
+height, width = size(image)
+plt.xlim([width, 0])
+plt.ylim([height, 0])
+plt.plot(parts[:, 1], parts[:, 2], "ko")
+plt.show()
